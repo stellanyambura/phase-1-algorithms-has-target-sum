cbc60081292789d5586e5c89226dc62109c422fe
Stray Gee <stellanyambura>
2022-12-08 21:23:54 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+       else if (array[j] + array[j] === target || array[i]+array[i]===target) {
+        return false;
+      } 
+      else if (array.length === 1) {
+        return false;
+      }
+    }
+  }
 }
 
 /* 
   Write the Big O time complexity of your function here
+  o(n²)
 */
 
 /* 
   Add your pseudocode here
+  To find two numbers that add to target, for every number i in the array we try to add number j
+   to i and see if that equals target. If it is, return the pair. 
+   The pairs should not be of the same index and of one lenght
 */
 
 /*
   Add written explanation of your solution here
+  loop through the the inner array to add each pair in the array then compare with the target in the outer array and compare if it 
+  equates
 */
 
 // You can run `node index.js` to view these console logs
